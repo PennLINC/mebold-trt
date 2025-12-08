7c8ee41daf6cf731c30a428bd9ba512df3a395c2
Taylor Salo <salot@pennmedicine.upenn.edu>
2025-12-08 13:08:14 -0500

    Fix paths.

--- processing/run_tedana.py
+++ processing/run_tedana.py
@@ -120,10 +120,10 @@ def run_tedana(raw_dir, fmriprep_dir, temp_dir, tedana_out_dir):
 
 
 if __name__ == "__main__":
-    raw_dir_ = "/cbica/projects/executivefunction/mebold-trt/dset"
-    fmriprep_dir_ = "/cbica/projects/executivefunction/mebold-trt/derivatives/fmriprep"
-    temp_dir_ = "/cbica/comp_space/executivefunction/tedana_temp"
-    tedana_out_dir_ = "/cbica/projects/executivefunction/mebold-trt/derivatives/tedana"
+    raw_dir_ = "/cbica/projects/executive_function/mebold-trt/dset"
+    fmriprep_dir_ = "/cbica/projects/executive_function/mebold-trt/derivatives/fmriprep"
+    temp_dir_ = "/cbica/comp_space/executive_function/tedana_temp"
+    tedana_out_dir_ = "/cbica/projects/executive_function/mebold-trt/derivatives/tedana"
 
     os.makedirs(temp_dir_, exist_ok=True)
 
