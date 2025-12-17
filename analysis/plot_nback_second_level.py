cdd4e0b23c4f8860ef2e3097dad5fe991960c9e8
Taylor Salo <salot@pennmedicine.upenn.edu>
2025-12-17 14:34:59 -0500

    Update plot_nback_second_level.py

--- analysis/plot_nback_second_level.py
+++ analysis/plot_nback_second_level.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         in_file,
         bg_img=bg_img,
         display_mode="mosaic",
-        threshold=0.01,
+        threshold=0.5,
         colorbar=True,
         symmetric_cbar=True,
         draw_cross=False,
