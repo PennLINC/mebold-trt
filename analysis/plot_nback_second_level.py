39c3027d4d246f734464290687784f72133ceb1d
Taylor Salo <salot@pennmedicine.upenn.edu>
2025-12-17 14:52:00 -0500

    Update plot_nback_second_level.py

--- analysis/plot_nback_second_level.py
+++ analysis/plot_nback_second_level.py
@@ -1,6 +1,11 @@
 """Plot the correlation matrices for the XCP-D outputs."""
 
+import nibabel as nb
+import numpy as np
 from nilearn import plotting
+from nilearn.image import get_data
+from nilearn.image.resampling import reorder_img
+from scipy.ndimage import binary_fill_holes
 
 
 if __name__ == "__main__":
@@ -13,9 +18,16 @@ if __name__ == "__main__":
         "/cbica/projects/executive_function/.cache/templateflow/"
         "tpl-MNI152NLin6Asym/tpl-MNI152NLin6Asym_res-02_T1w.nii.gz"
     )
+    bg_img = nb.load(bg_img)
+    bg_img = reorder_img(bg_img)
+    data = get_data(bg_img)
+    data = data.astype(np.float64)
+    anat_mask = binary_fill_holes(data > np.finfo(float).eps)
+    data = np.ma.masked_array(data, np.logical_not(anat_mask))
+    bg_img = nb.Nifti1Image(data, bg_img.affine, bg_img.header)
     plotting.plot_stat_map(
         in_file,
-        #bg_img=bg_img,
+        bg_img=bg_img,
         display_mode="mosaic",
         threshold=1.96,
         colorbar=True,
