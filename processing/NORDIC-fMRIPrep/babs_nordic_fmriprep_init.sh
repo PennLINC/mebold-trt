678ace9c75f2b573ae91aea217b1a06701e11d55
Taylor Salo <salot@pennmedicine.upenn.edu>
2026-03-31 15:08:30 -0400

    Fix paths.

--- processing/NORDIC-fMRIPrep/babs_nordic_fmriprep_init.sh
+++ processing/NORDIC-fMRIPrep/babs_nordic_fmriprep_init.sh
@@ -6,7 +6,7 @@ export TEMPLATEFLOW_HOME=/cbica/projects/executive_function/mebold_trt/templatef
 babs init \
   --container_ds /cbica/projects/executive_function/mebold_trt/software/apptainer-ds/nordic-fmriprep-ds \
   --container_name nordic-0-0-1 \
-  --container_config /cbica/projects/executive_function/mebold_trt/derivatives/code/babs_nordic_fmriprep.yaml \
+  --container_config /cbica/projects/executive_function/mebold_trt/github/parker/processing/NORDIC-fMRIPrep/babs_nordic_fmriprep.yaml \
   --processing_level subject \
   --queue slurm \
   /cbica/projects/executive_function/mebold_trt/derivatives/nordic_fmriprep_babs_project
